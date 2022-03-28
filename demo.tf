edae30348f2210f6f348ba3d2293fe55d320b8f0
Chandan Gulati <33059635+Chandang786@users.noreply.github.com>
2022-03-28 17:06:40 +0530

    create demo.tf

---
+++ demo.tf
@@ -0,0 +1,18 @@
+
+data "aws_ami" "app_ami" {
+
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+
+  }
+}
+
+resource "aws_instance" "myweb" {
+    ami = data.aws_ami.app_ami.id
+    #"00ee4df451840fa9d"
+    instance_type = var.instance_size
+}
